050f4bcb7db8724420ca0241805192c428418e76
LQX-NWU <nwulqx@foxmail.com>
2021-06-29 08:26:28 +0800

    feat: 文件上传

--- video/src/main/java/com/vms/video/VideoApplication.java
+++ video/src/main/java/com/vms/video/VideoApplication.java
@@ -1,30 +1,30 @@
-package com.vms.video;
-
-import java.lang.System;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Controller;
-
-@SpringBootApplication
-@Controller
-public class VideoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VideoApplication.class, args);
-	}
-
-	@RequestMapping("/")
-	public String idx() {
-		return "index";
-	}
-
-	@RequestMapping("/t")
-	public String test() {
-		return "test";
-	}
-
-}
-
+//package com.vms.video;
+//
+//import java.lang.System;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.stereotype.Controller;
+//
+//@SpringBootApplication
+//@Controller
+//public class VideoApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(VideoApplication.class, args);
+//	}
+//
+//	@RequestMapping("/")
+//	public String idx() {
+//		return "index";
+//	}
+//
+//	@RequestMapping("/t")
+//	public String test() {
+//		return "test";
+//	}
+//
+//}
+//
